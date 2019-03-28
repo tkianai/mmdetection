ed6721a26142f550bd0b7510fb9dea9bdd9a7a16
tiankai <tiankai@Loading.local>
2019-03-28 10:32:51 +0800

    add lsvt config

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -6,3 +6,4 @@ $PYTHON -m torch.distributed.launch --nproc_per_node=$2 $(dirname "$0")/train.py
 
 
 # python -m torch.distributed.launch --nproc_per_node=8 --nnodes=2 --node_rank=0 --master_addr="10.141.8.84" --master_port=9876 tools/train.py configs/cascade_mask_rcnn_x101_64x4d_fpn_1x.py --launcher pytorch
+# python -m torch.distributed.launch --nproc_per_node=8 --nnodes=2 --node_rank=1 --master_addr="10.141.8.84" --master_port=9876 tools/train.py configs/cascade_mask_rcnn_x101_64x4d_fpn_1x.py --launcher pytorch
