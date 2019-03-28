5cc4f8028b1b1773cb9a90e572a44a45bd2fd888
tiankai <tiankai@Loading.local>
2019-03-28 14:24:00 +0800

    parameters tuning

--- tools/trim_detection_model.py
+++ tools/trim_detection_model.py
@@ -1,53 +1,24 @@
 import os
-import torch
 import argparse
-from mmcv import Config
 
-from mmdet import __version__
-from mmdet.datasets import get_dataset
-from mmdet.apis import (train_detector, init_dist, get_root_logger,
-                        set_random_seed)
-from mmdet.models import build_detector
+import torch
 
-
-def removekey(d, listofkeys):
-    r = dict(d)
-    for key in listofkeys:
-        print('key: {} is removed'.format(key))
-        r.pop(key)
-    return r
-
-
-parser = argparse.ArgumentParser(
-    description="Trim Detection weights and save in PyTorch format.")
-parser.add_argument(
-    "--pretrained_path",
-    default="~/.torch/models/_detectron_35858933_12_2017_baselines_e2e_mask_rcnn_R-50-FPN_1x.yaml.01_48_14.DzEQe4wC_output_train_coco_2014_train%3Acoco_2014_valminusminival_generalized_rcnn_model_final.pkl",
-    help="path to detectron pretrained weight(.pkl)",
-    type=str,
-)
-parser.add_argument(
-    "--save_path",
-    default="./pretrained_model/mask_rcnn_R-50-FPN_1x_detectron_no_last_layers.pth",
-    help="path to save the converted model",
-    type=str,
-)
-parser.add_argument(
-    "--cfg",
-    default="configs/e2e_mask_rcnn_R_50_FPN_1x.yaml",
-    help="path to config file",
-    type=str,
-)
+parser = argparse.ArgumentParser()
+parser.add_argument('--pretrained-path', help='pretrained model path')
+parser.add_argument('--save-path', help='the path to save transfered model')
 
 args = parser.parse_args()
-#
-DETECTRON_PATH = os.path.expanduser(args.pretrained_path)
-print('detectron path: {}'.format(DETECTRON_PATH))
 
-_d = torch.load(DETECTRON_PATH)
-_d = _d['state_dict']
+origin_model = torch.load(args.pretrained_path)
+origin_model = origin_model['state_dict']
 
-newdict = removekey(_d,
-                             ['fc_cls.bias', 'fc_cls.weight', 'fc_reg.bias', 'fc_reg.weight'])
+# remove the unfitted layers
+removed_keys = ['fc_cls', 'fc_reg']
+newdict = origin_model
+for key in origin_model.keys():
+    for removed_key in removed_keys:
+        if removed_key in key:
+            newdict.pop(key)
+            break
+
 torch.save(newdict, args.save_path)
-print('saved to {}.'.format(args.save_path))
