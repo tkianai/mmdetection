b3c115e48dc1782a3dc3390d6dd3c486d7033727
tiankai <tiankai@Loading.local>
2019-03-28 10:25:51 +0800

    revise parameters

--- tools/dist_train.sh
+++ tools/dist_train.sh
@@ -3,3 +3,6 @@
 PYTHON=${PYTHON:-"python"}
 
 $PYTHON -m torch.distributed.launch --nproc_per_node=$2 $(dirname "$0")/train.py $1 --launcher pytorch ${@:3}
+
+
+# python -m torch.distributed.launch --nproc_per_node=8 --nnodes=2 --node_rank=0 --master_addr="10.141.8.84" --master_port=9876 tools/train.py configs/cascade_mask_rcnn_x101_64x4d_fpn_1x.py --launcher pytorch
