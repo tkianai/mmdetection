16f443434a33b84ff7c1f357470edc19202cdc90
tiankai <tiankai@Loading.local>
2019-03-28 19:55:04 +0800

    parameters tuning

--- configs/cascade_mask_rcnn_x101_64x4d_fpn_1x_lsvt.py
+++ configs/cascade_mask_rcnn_x101_64x4d_fpn_1x_lsvt.py
@@ -201,7 +201,7 @@ data = dict(
         with_label=False,
         test_mode=True))
 # optimizer
-optimizer = dict(type='SGD', lr=0.02, momentum=0.9, weight_decay=0.0001)
+optimizer = dict(type='SGD', lr=0.04, momentum=0.9, weight_decay=0.0001)
 optimizer_config = dict(grad_clip=dict(max_norm=35, norm_type=2))
 # learning policy
 lr_config = dict(
@@ -220,7 +220,7 @@ log_config = dict(
     ])
 # yapf:enable
 # runtime settings
-total_epochs = 12
+total_epochs = 20
 dist_params = dict(backend='nccl')
 log_level = 'INFO'
 work_dir = './work_dirs/cascade_mask_rcnn_x101_64x4d_fpn_1x_lsvt/normal'
